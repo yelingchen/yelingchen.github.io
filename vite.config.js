fd0dabd4c5798ac6531c7cd3f34af7dc101f88bf
yelingchen <83534873@qq.com>
2024-05-22 20:01:48 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/yelingchen.github.io/', // 设置基路径
+  base: '/',  // 设置为你的仓库名称
   plugins: [
     vue(),
   ],
